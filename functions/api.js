e4aac3f28997c3ece318f2b6ec8efd931424723a
reinforest915820 <alexandre915820@gmail.com>
2022-12-05 12:53:49 -0500

    complete the netlify task

--- functions/api.js
+++ functions/api.js
@@ -37,13 +37,12 @@ app.use("/", router);
 const connection_string = process.env.CONNECTION_STRING;
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}...`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}...`);
+// });
 
-mongoose
-  .connect(connection_string)
-  .then(() => console.log("MongDB connection established..."))
-  .catch((error) => console.error("MongDB connection failed:", error.message));
+mongoose.connect(connection_string);
+// .then(() => console.log("MongDB connection established..."))
+// .catch((error) => console.error("MongDB connection failed:", error.message));
 
 module.exports.handler = serverless(app);
